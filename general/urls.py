cb6019e841d404cbcca3fbae5ea3c535648ab8ac
Steffyn <kshernandez90@gmail.com>
2016-10-10 00:33:45 -0600

    url

--- general/urls.py
+++ general/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	url(r'^logout/$', views.cerrar_sesion, name='cerrar_sesion'),
 	url(r'^crear/usuario/$', views.crear_usuario, name='crear_usuario'),
 	url(r'^principal/$', views.principal, name='principal'),
+	url(r'^registrate/$', views.registrate, name='registrate'),
 ]
